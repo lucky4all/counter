007b6236b817083420d4e9b9bab4e9cad6e083c0
lucky4all <lucasromerocoscia@gmail.com>
2025-06-05 09:41:27 -0300

    Se añadió un botón de resetear contador

--- src/App.jsx
+++ src/App.jsx
@@ -15,11 +15,16 @@ export function Contador() {
       setClick(click + 1)
     }
 
+    function handleReset() {
+      setClick(0)
+    }
+
     return (
         <div id="clicker">
           <h1>Contador <span>EPICO!</span>!</h1>
           <h2>Cantidad de clicks: {click}</h2>
           <button onClick={handleClick}>Haceme Click!</button>
+          <button id='reset' onClick={handleReset}>Resetear contador</button>
         </div>
       )
     
