021a8fd015ffe37b027fb131b0d50e65b4299f46
lucky4all <lucasromerocoscia@gmail.com>
2025-06-09 16:59:24 -0300

    feat: Rediseño total de la estética

--- src/App.jsx
+++ src/App.jsx
@@ -29,12 +29,12 @@ export function Contador() {
     }
 
     return (
-        <div id="clicker">
-          <h1>Contador <span>EPICO!</span>!</h1>
-          <h2>Cantidad de clicks: {click}</h2>
-          <button onClick={handleClick}>Sumar Click!</button>
-          <button id='reset' onClick={handleReset}>Resetear contador</button>
-          <button id='down' onClick={handleDecrement}>Restar un click</button>
+        <div id="clicker" className='flex flex-col w-96 h-86 items-center justify-center gap-5 border-4 border-purple-500'>
+          <h1 className='text-4xl text-white font-mono'>Contador <span className='text-pink-300 text-shadow-2xs text-shadow-black'>EPICO!</span>!</h1>
+          <h2 className='text-xl'>Cantidad de clicks: <span className='text-purple-400 shadow-2xl font-bold'>{click}</span></h2>
+          <button id='up' onClick={handleClick} className='bg-purple-400 text-white py-1 px-1 rounded-3xl text-[18px] font-bold cursor-pointer hover:bg-purple-500 order-1 transition-colors'>Sumar Click!</button>
+          <button id='reset' onClick={handleReset} className='bg-white text-black py-1 px-2 rounded-3xl text-[18px] font-bold cursor-pointer order-3'>Resetear contador</button>
+          <button id='down' onClick={handleDecrement} className='bg-pink-300 text-white py-1 px-2 rounded-3xl text-[18px] font-bold cursor-pointer order-2 hover:bg-pink-400 transition-colors'>Restar un click</button>
         </div>
       )
     
